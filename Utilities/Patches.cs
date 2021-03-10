6bda70cdc43924b9828851a0c36e64508fbef439
Korty (Lily) <admin@kortyboi.com>
2021-03-10 00:49:42 -0500

    Update Patches.cs

--- Utilities/Patches.cs
+++ Utilities/Patches.cs
@@ -11,18 +11,16 @@ namespace ComponentToggle.Utilities
 {
     class Patches
     {
-        private static HarmonyMethod GetLocalPatch(string name) { return new HarmonyMethod(typeof(Patches).GetMethod(name, BindingFlags.Static | BindingFlags.NonPublic)); }
-
         private static HarmonyInstance ComponentTogglePatch = HarmonyInstance.Create("ComponentToggle");
 
         public static void PatchVRC_Station()
         {
-            ComponentTogglePatch.Patch(AccessTools.Property(typeof(VRC_StationInternal), "Method_Public_Boolean_Player_Boolean_0").GetMethod, null, GetLocalPatch("CanUseStation"), null);
+            ComponentTogglePatch.Patch(typeof(VRC_StationInternal).GetMethod("Method_Public_Boolean_Player_Boolean_0"), new HarmonyMethod(typeof(Patches).GetMethod("CanUseStation", BindingFlags.Static | BindingFlags.NonPublic)), null, null);
         }
 
         private static bool CanUseStation(ref bool __result, VRC.Player __0, bool __1)
         {
-            if (__0 != null && __0 == VRCPlayer.field_Internal_Static_VRCPlayer_0.field_Private_Player_0 && !CustomConfig.Get().VRC_Station)
+            if (__0 != null && __0 == VRCPlayer.field_Internal_Static_VRCPlayer_0.field_Private_Player_0 && CustomConfig.Get().VRC_Station)
             {
                 __result = false;
                 return false;
