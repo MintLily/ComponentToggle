2cd9c237485ff29a7d40606744f95d1de0638ad9
Korty (Lily) <admin@kortyboi.com>
2021-03-11 16:33:38 -0500

    Fixed Reversed bool

--- Utilities/Patches.cs
+++ Utilities/Patches.cs
@@ -15,12 +15,13 @@ namespace ComponentToggle.Utilities
 
         public static void PatchVRC_Station()
         {
-            ComponentTogglePatch.Patch(typeof(VRC_StationInternal).GetMethod("Method_Public_Boolean_Player_Boolean_0"), new HarmonyMethod(typeof(Patches).GetMethod("CanUseStation", BindingFlags.Static | BindingFlags.NonPublic)), null, null);
+            ComponentTogglePatch.Patch(typeof(VRC_StationInternal).GetMethod("Method_Public_Boolean_Player_Boolean_0"),
+                new HarmonyMethod(typeof(Patches).GetMethod("CanUseStation", BindingFlags.Static | BindingFlags.NonPublic)), null, null);
         }
 
         private static bool CanUseStation(ref bool __result, VRC.Player __0, bool __1)
         {
-            if (__0 != null && __0 == VRCPlayer.field_Internal_Static_VRCPlayer_0.field_Private_Player_0 && CustomConfig.Get().VRC_Station)
+            if (__0 != null && __0 == VRCPlayer.field_Internal_Static_VRCPlayer_0.field_Private_Player_0 && !CustomConfig.Get().VRC_Station)
             {
                 __result = false;
                 return false;
