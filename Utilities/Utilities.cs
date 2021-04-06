d82287fdb25f993d7b1be360f2fa25acba6e9f57
Korty (Lily) <admin@kortyboi.com>
2021-04-06 12:49:38 -0400

    Added UiToolTip to support Rich Text

--- Utilities/Utilities.cs
+++ Utilities/Utilities.cs
@@ -1,24 +1,36 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 using VRC.Core;
 
 namespace ComponentToggle.Utilities
 {
     public class RoomExtensions
     {
-        public static bool IsInWorld()
-        {
-            return GetWorld() != null || GetWorldInstance() != null;
-        }
+        public static bool IsInWorld() { return GetWorld() != null || GetWorldInstance() != null; }
 
-        public static ApiWorld GetWorld()
-        {
-            return RoomManager.field_Internal_Static_ApiWorld_0;
-        }
+        public static ApiWorld GetWorld() { return RoomManager.field_Internal_Static_ApiWorld_0; }
 
-        public static ApiWorldInstance GetWorldInstance()
+        public static ApiWorldInstance GetWorldInstance() { return RoomManager.field_Internal_Static_ApiWorldInstance_0; }
+    }
+
+    public class Menu
+    {
+        public static IEnumerator AllowToolTipTextColor()
         {
-            return RoomManager.field_Internal_Static_ApiWorldInstance_0;
+            try
+            {
+                GameObject TooltipText = GameObject.Find("UserInterface/QuickMenu/QuickMenu_NewElements/_CONTEXT/QM_Context_ToolTip/_ToolTipPanel/Text");
+                TooltipText.GetComponentInChildren<Text>().supportRichText = true;
+            }
+            catch { MelonLoader.MelonLogger.Error("Failed to make ToolipText supportRichText"); }
+            try
+            {
+                GameObject ALTTooltipText = GameObject.Find("UserInterface/QuickMenu/QuickMenu_NewElements/_CONTEXT/QM_Context_User_Hover/_ToolTipPanel/Text");
+                ALTTooltipText.GetComponentInChildren<Text>().supportRichText = true;
+            }
+            catch { MelonLoader.MelonLogger.Error("Failed to make ALTTooltipText supportRichText"); }
+            yield break;
         }
     }
 }
